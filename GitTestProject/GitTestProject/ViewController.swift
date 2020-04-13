46616e2972aa2bfc6720b454c30737a5f0e30417
MEHMET NARLIOGLU <mnarlioglu@gmail.com>
2020-04-13 17:54:09 +0300

    Add files via upload

---
+++ GitTestProject/GitTestProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Mehmet NARLIOĞLU on 13.04.2020.
+//  Copyright © 2020 Entroopica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("aq")
+        print("aqq")
+    }
+
+
+}
+
---
+++ GitTestProject/README.md
@@ -0,0 +1,2 @@
+# EnTest
+# EnTest
